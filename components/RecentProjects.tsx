5dacdd7e42f3bfe6bdabc0ea508ecb697aea7538
andreaprocopio <andreproco@gmail.com>
2025-05-19 15:56:25 +0200

    Correcting mobile of projects section

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -21,7 +21,7 @@ export function RecentProjects() {
           title="ClarityFlow"
           href="https://clarity-flow-seven.vercel.app/"
         >
-          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] md:w-[25rem]  h-[25rem]">
+          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[16rem] md:w-[25rem]  h-[25rem]">
             <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
               ClarityFlow
             </h3>
@@ -62,7 +62,7 @@ export function RecentProjects() {
           title="Garden of Gethsemane"
           href="https://garden-of-gethsemane.vercel.app/"
         >
-          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] md:w-[25rem] h-[25rem]">
+          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[16rem] md:w-[25rem] h-[25rem]">
             <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
               Garden of Gethsemane
             </h3>
