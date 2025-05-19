7a1967223ce5567d25b21e368ff1f85fbdf0abca
andreaprocopio <andreproco@gmail.com>
2025-05-19 18:53:10 +0200

    lowering hero height

--- components/ui/background-beams-with-collision.tsx
+++ components/ui/background-beams-with-collision.tsx
@@ -70,7 +70,7 @@ export const BackgroundBeamsWithCollision = ({
     <div
       ref={parentRef}
       className={cn(
-        "min-h-screen bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-background relative flex items-center w-full justify-center overflow-hidden",
+        "min-h-[20rem] md:min-h-[30rem] bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-background relative flex items-center w-full justify-center overflow-hidden",
         // h-screen if you want bigger
         className
       )}
